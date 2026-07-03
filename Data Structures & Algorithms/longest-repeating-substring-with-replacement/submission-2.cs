b18cb1b0aad71979f79fd38628f0de19dd9969bf
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-07-03 16:51:14 +0300

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int[] count = new int[26];
+
+        int maxCount = 0;
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0 ; right < s.Length ; right++){
+            count[s[right]-'A']++;
+            maxCount = Math.Max(maxCount, count[s[right] - 'A']);
+
+            if ((right - left + 1 ) - maxCount > k){
+                count[s[left]-'A']--;
+                left++;
+            }
+            maxLength = Math.Max(maxLength, (right - left + 1 ));
+        }   
+
+        return maxLength;
+    }
+}
