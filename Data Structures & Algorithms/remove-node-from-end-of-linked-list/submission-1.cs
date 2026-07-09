f2838a319572bd0eee49eeeefaf0a1e7af06dd95
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-07-09 16:04:04 +0300

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head, prev = null;
+        int count = 0;
+
+        while (count != n) {
+            count++;
+            curr = curr.next;
+        }
+        prev = dummy;
+        ListNode temp = head;
+        while (curr != null) {
+            prev = temp;
+            curr = curr.next;
+            temp = temp.next;
+        }
+
+        ListNode temp2 = temp.next;
+        temp.next = null;
+        prev.next = temp2;
+        
+        return dummy.next;
+    }
+}
