d01f862d798f1196e62c77ec75b8982bc592b98a
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-07-23 16:41:42 +0300

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    int count = 0;
+    int result = -1000;
+    public int KthSmallest(TreeNode root, int k) {
+
+        if ( root == null ) return -1;
+        if ( result != -1000) {
+            return result;
+        }
+
+        KthSmallest(root.left, k);
+
+        count++;
+        if ( count == k ) {
+            result = root.val;
+            return result;
+        }
+
+        KthSmallest(root.right, k);
+
+        return result;
+    }
+}
