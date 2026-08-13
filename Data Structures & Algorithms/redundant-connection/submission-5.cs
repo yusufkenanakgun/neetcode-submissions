6434f6d29b65d0adfe53792c4d839285a87a7916
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-08-13 13:03:59 +0300

    Add: redundant-connection - submission-5

---
+++ Data Structures & Algorithms/redundant-connection/submission-5.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    int[] parent;
+    int[] size;
+    public int[] FindRedundantConnection(int[][] edges) {
+        int n = edges.Length;
+
+        parent = new int[n + 1];
+        size = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        foreach ( var e in edges) {
+            if (!Union(e[0], e[1])) {
+                return e;
+            }
+        }
+        return new int[0];
+    }
+    private int Find(int x) {
+        if (parent[x] != x) {
+            parent[x] = Find(parent[x]);
+        }
+        return parent[x];
+    }
+    private bool Union(int a, int b) {
+        int rootA = Find(a);
+        int rootB = Find(b);
+
+        if (rootA == rootB) return false;
+        if (size[rootA] < size[rootB]) {
+            (rootA, rootB) = (rootB, rootA);
+        }
+        parent[rootB] = rootA;
+        size[rootA] += size[rootB];
+        return true;
+    }
+
+}
