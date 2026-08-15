8ebbf5199093c3b0cb15570001f4a1860e7d958e
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-08-15 20:17:19 +0300

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<int>> Subsets(int[] nums) {
+        List<int> list = new();
+        List<List<int>> result = new();
+
+        return CreateSubsets(nums, 0, list, result);
+    }
+
+    List<List<int>> CreateSubsets(int[] n, int i, List<int> l, List<List<int>> r) {
+        r.Add(new List<int>(l));
+
+        while (i < n.Length) {
+            l.Add(n[i]);
+            CreateSubsets(n, i + 1, l, r);
+            l.RemoveAt(l.Count - 1);
+            i++;
+        }
+        
+        
+        return r;
+    }
+}
