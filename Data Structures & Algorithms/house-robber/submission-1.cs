bb487221fc5dbd8e93aef26a19c866730ef87711
yusufkenanakgun <yusufkenanakgun@gmail.com>
2026-08-18 15:21:34 +0300

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        Dictionary<int, int> map = new();
+        return Robb(map, nums, nums.Length - 1);        
+    }
+
+    private int Robb(Dictionary<int, int> m, int[] n, int c) {
+        if (m.ContainsKey(c)) return m[c];
+        if (c < 0) return 0;
+        m[c] = Math.Max(Robb(m, n, c - 1), Robb(m, n, c - 2) + n[c]);
+        return m[c];
+    }
+}
